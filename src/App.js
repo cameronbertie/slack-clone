0d3ab50a5e2f327d7129c152c3fdae2a3e1f8f6d
Cameron Bertie <72890283+cameronbertie@users.noreply.github.com>
2022-11-03 19:19:01 +0000

    Built the header component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
 import React from "react";
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import styled from "styled-components";
 
 function App() {
   return (
     <div className="app">
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<h1>This is the homepage</h1>} exact>
+      <Header />
+      <AppBody>
+      <Routes>
+          <Route path="/" element={<Header />} exact>
           </Route>
         </Routes>
+      </AppBody>
       </BrowserRouter>
     </div>
   );
 }
 
 export default App;
+
+const AppBody = styled.div`
+
+`
