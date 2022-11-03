2594cdd4a29e6422b8e57ab9a6fa0c308b38b86e
Cameron Bertie <72890283+cameronbertie@users.noreply.github.com>
2022-11-03 17:52:30 +0000

    Set up firebase

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDpQbb-_98NgjwZfLaCqmG0aL5QEguuMhI",
+    authDomain: "slack-clone-dbc97.firebaseapp.com",
+    projectId: "slack-clone-dbc97",
+    storageBucket: "slack-clone-dbc97.appspot.com",
+    messagingSenderId: "1045290035361",
+    appId: "1:1045290035361:web:dd6aba5d3b7dc9f2d0a8e0"
+  };
