b3787b2224960aee7c086676f85e87625430a9d7
Cameron Bertie <72890283+cameronbertie@users.noreply.github.com>
2022-11-05 14:40:48 +0000

    Built the sidebar option component

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,9 +1,26 @@
 import FiberManualRecordIcon from "@mui/icons-material/FiberManualRecord";
 import React from "react";
 import styled from "styled-components";
-import CreateIcon from '@mui/icons-material/Create';
+import CreateIcon from "@mui/icons-material/Create";
+import InsertCommentIcon from "@mui/icons-material/InsertComment";
+import InboxIcon from "@mui/icons-material/Inbox";
+import DraftsIcon from "@mui/icons-material/Drafts";
+import BookmarkBorderIcon from "@mui/icons-material/BookmarkBorder";
+import PeopleAltIcon from "@mui/icons-material/PeopleAlt";
+import AppsIcon from "@mui/icons-material/Apps";
+import FolderCopyIcon from "@mui/icons-material/FolderCopy";
+import ExpandLessIcon from "@mui/icons-material/ExpandLess";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import AddIcon from "@mui/icons-material/Add";
+import SidebarOption from "./SidebarOption";
+import { db, collection } from "../firebase";
+import { useCollection } from 'react-firebase-hooks/firestore';
 
 function Sidebar() {
+  const [channels, loading, error] = useCollection(collection(db, 'rooms'));
+  
+
+
   return (
     <SidebarContainer>
       <SidebarHeader>
@@ -17,7 +34,22 @@ function Sidebar() {
         <CreateIcon />
       </SidebarHeader>
 
-      
+      <SidebarOption Icon={InsertCommentIcon} title="Threads" />
+      <SidebarOption Icon={InboxIcon} title="Mentions & reactions" />
+      <SidebarOption Icon={DraftsIcon} title="Saved items" />
+      <SidebarOption Icon={BookmarkBorderIcon} title="Channel browser" />
+      <SidebarOption Icon={PeopleAltIcon} title="People & user groups" />
+      <SidebarOption Icon={AppsIcon} title="Apps" />
+      <SidebarOption Icon={FolderCopyIcon} title="File Browser" />
+      <SidebarOption Icon={ExpandLessIcon} title="Show less" />
+      <hr />
+      <SidebarOption Icon={ExpandMoreIcon} title="Channels" />
+      <hr />
+      <SidebarOption Icon={AddIcon} addChannelOption title="Add Channel" />
+
+      {channels?.docs.map(doc => (
+        <SidebarOption key={doc.id} id={doc.id} title={doc.data().name} />
+      ))}
     </SidebarContainer>
   );
 }
@@ -31,43 +63,49 @@ const SidebarContainer = styled.div`
   border-top: 1px solid #49274b;
   max-width: 260px;
   margin-top: 60px;
+
+  > hr {
+    margin-top: 10px;
+    margin-bottom: 10px;
+    border: 1px solid #49274b;
+  }
 `;
 
 const SidebarHeader = styled.div`
-display: flex;
-border-bottom: 1px solid #49274b;
-padding-bottom: 10px;
-padding: 13px;
+  display: flex;
+  border-bottom: 1px solid #49274b;
+  padding-bottom: 10px;
+  padding: 13px;
 
-> .MuiSvgIcon-root {
+  > .MuiSvgIcon-root {
     padding: 8px;
     color: #49274b;
     font-size: 18px;
     background-color: white;
     border-radius: 999px;
-}
+  }
 `;
 
 const SidebarInfo = styled.div`
-flex: 1;
+  flex: 1;
 
-> h2 {
+  > h2 {
     font-size: 15px;
     font-weight: 900;
     margin-bottom: 5px;
-}
+  }
 
-> h3 {
+  > h3 {
     display: flex;
     font-size: 13px;
     font-weight: 400;
     align-items: center;
-}
+  }
 
-> h3 > .MuiSvgIcon-root {
+  > h3 > .MuiSvgIcon-root {
     font-size: 14px;
     margin-top: 1px;
     margin-right: 2px;
     color: green;
-}
+  }
 `;
--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,8 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore, collection, addDoc } from 'firebase/firestore';
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+
+
 const firebaseConfig = {
     apiKey: "AIzaSyDpQbb-_98NgjwZfLaCqmG0aL5QEguuMhI",
     authDomain: "slack-clone-dbc97.firebaseapp.com",
@@ -6,3 +11,10 @@ const firebaseConfig = {
     messagingSenderId: "1045290035361",
     appId: "1:1045290035361:web:dd6aba5d3b7dc9f2d0a8e0"
   };
+
+  const app = initializeApp(firebaseConfig);
+  const db = getFirestore(app);
+  const auth = getAuth(app);
+  const provider = new GoogleAuthProvider();
+
+  export { auth, provider, db, collection, addDoc };
